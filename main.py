bf50b0684cc9deea305f9d0eedaa8615ed479ed0
Hashim <109148083+5wHN28Dg@users.noreply.github.com>
2025-08-07 19:00:11 +0300

    scanning chats for new messages
    scanning, added keywords, removing irrelevant code snippets

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 
 # Use your own values from my.telegram.org
-api_id = 98534905
-api_hash = 'ff'
+api_id = 0
+api_hash = '0'
 client = TelegramClient('test', api_id, api_hash)
+chats = ['jobsalbasra', 'basrajobs', 'basrajobs3', 'jobbbs', 'basrajobs_ads', 'basrahvacancies',
+        'vacancies_training', 'jobs_basra1', 'engineers_jobs_basra', 'basra_job', 'alisaedhsn',
+        'Jobs_Advices', 'engahmad88', 'jobs_iraq1', 'Muhannad_job', 'Iraq_careers', 'YSPjobs',
+        'jobs_for_us', 'iraq_jobs2', 'eng7600', 'iqjscout', 'Eng_job25', 'iraqjobs1995', 'iraqi_HR',
+        'jobs_free']
+level_keywords = [
+    "intern", "internship", "trainee", "junior",
+    "entry level", "graduate", "co-op"
+]
+role_keywords = [
+    "help desk", "it support", "it technician", "network support",
+    "noc technician", "desktop support", "system admin",
+    "systems analyst", "data center technician", "pc technician",
+    "control systems engineer", "automation engineer",
+    "embedded systems", "junior developer", "software intern",
+    "cybersecurity technician"]
+location_keywords = ['remote', 'basra', 'بصرة', 'عن بعد', 'ام قصر']
+
+@client.on(events.NewMessage(chats))
+async def new_message_handler(event):
+    if 'hello' in event.raw_text:
+        await event.reply('hi!')
 
 async def main():
-    # Getting information about yourself
-    me = await client.get_me()
-
-    # "me" is a user object. You can pretty-print
-    # any Telegram object with the "stringify" method:
-    print(me.stringify())
-
-    # When you print something, you see a representation of it.
-    # You can access all attributes of Telegram objects with
-    # the dot operator. For example, to get the username:
-    username = me.username
-    print(username)
-    print(me.phone)
 
     # You can print all the dialogs/conversations that you are part of:
     async for dialog in client.iter_dialogs():
         print(dialog.name, 'has ID', dialog.id)
 
-    # You can send messages to yourself...
-    await client.send_message(
-        'me',
-        'This message has **bold**, `code`, __italics__ and '
-        'a [nice website](https://example.com)!',
-        link_preview=False
-    )
     # ...to some chat ID
     await client.send_message('placeholder', 'Hello, group!')
-    # ...to your contacts
-    await client.send_message('placeholder', 'test 123')
 
 with client:
     client.loop.run_until_complete(main())
