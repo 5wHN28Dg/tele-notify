7701a052db7ccc1034cd8c4f7a67aa2ea737d786
Hashim <109148083+5wHN28Dg@users.noreply.github.com>
2026-01-23 21:17:04 +0300

    okay ruff

--- get_chat_ids.py
+++ get_chat_ids.py
@@ -1,13 +1,15 @@
 from telethon import TelegramClient
 
 api_id = YOUR_API_ID
-api_hash = 'YOUR_API_HASH'
+api_hash = "YOUR_API_HASH"
 
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient("session_name", api_id, api_hash)
+
 
 async def main():
     async for dialog in client.iter_dialogs():
-        print('{:>14}: {}'.format(dialog.id, dialog.title))
+        print("{:>14}: {}".format(dialog.id, dialog.title))
+
 
 with client:
- client.loop.run_until_complete(main())
+    client.loop.run_until_complete(main())
